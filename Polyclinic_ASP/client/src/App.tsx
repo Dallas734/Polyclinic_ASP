3fcef56661b9ba53244eb31c630f20f03d90d92a
Mark <vernidub66@gmail.com>
2024-02-19 22:37:21 +0300

    Made NavBar

--- Polyclinic_ASP/client/src/App.tsx
+++ Polyclinic_ASP/client/src/App.tsx
@@ -4,16 +4,20 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Doctor from './Components/Doctor/Doctor';
 import 'bootstrap/dist/css/bootstrap.css';
+import Layout from './Components/Layer/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Layout />}>
           <Route path='/doctors' element={
           <div>
             <Doctor />
           </div>
           }/>
+          <Route path='/addVisits'/>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ Polyclinic_ASP/client/src/Components/Layer/Layout.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {Outlet} from "react-router-dom";
+import {Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const Layout: React.FC = () =>{
+    return (
+    <>
+    <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+        <NavbarBrand tag={Link} to="/">Поликлиника</NavbarBrand>
+            <ul className="navbar-nav flex-grow">
+                <NavItem>
+                    <NavLink tag={Link} to='/'>Главная</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} to='/doctors'>Доктора</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={Link} to='/addVisits'>Записать пациента</NavLink>
+                </NavItem>
+                <NavItem>
+                        <NavLink tag={Link} to='/logoff'>Выход</NavLink>
+                </NavItem>
+            </ul>
+    </Navbar>
+    <Outlet />
+    </>
+    );
+}
+export default Layout;
