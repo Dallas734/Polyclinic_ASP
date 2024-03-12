1230b43552ff2c0cfa213d6730b7a6d948609d66
Mark <vernidub66@gmail.com>
2024-03-12 15:18:52 +0300

    Imroved design

--- Polyclinic_ASP/client/src/Components/Layer/Layout.tsx
+++ Polyclinic_ASP/client/src/Components/Layer/Layout.tsx
@@ -1,9 +1,46 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
-import {Navbar, NavbarBrand, NavItem, NavLink, Nav, DropdownMenu, DropdownToggle, UncontrolledDropdown, DropdownItem } from 'reactstrap';
+import {NavLink, DropdownMenu, DropdownToggle, UncontrolledDropdown, DropdownItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import UserObj from "../Entities/UserObj";
+import { Layout as LayoutAntd, Menu } from 'antd';
+
+const { Header, Content, Footer } = LayoutAntd;
+
+const registratorItems = [
+    {
+        label: <NavLink tag={Link} to="/">МИС</NavLink>,
+        key: "1",
+    },
+    {
+        label: <NavLink tag={Link} to="/doctors">Доктора</NavLink>,
+        key: "2",
+    },
+    {
+        label: <NavLink tag={Link} to="/patients">Пациенты</NavLink>,
+        key: "3",
+    },
+    {
+        label: <NavLink tag={Link} to="/addVisits">Записать пациента</NavLink>,
+        key: "4",
+    }
+];
+
+const doctorItems = [
+    {
+        label: <NavLink tag={Link} to="/">МИС</NavLink>,
+        key: "1",
+    },
+    {
+        label: <NavLink tag={Link} to="/patientsCard">Медкарта</NavLink>,
+        key: "2",
+    },
+    {
+        label: <NavLink tag={Link} to="/doctorsTalons">Принять пациента</NavLink>,
+        key: "3",
+    }
+];
 
 interface PropsType {
     user: UserObj | null
@@ -11,47 +48,28 @@ interface PropsType {
 
 const Layout: React.FC<PropsType> = ({ user }) => {
     return (
-    <>
-    <Navbar className="navbar navbar-expand-lg bg-dark navbar-dark" container>
-        <NavbarBrand tag={Link} to="/">МИС</NavbarBrand>
-            <Nav className="me-auto" navbar>
-                {user?.roles.includes("Registrator") ? (
-                    <>
-                    <NavItem>
-                        <NavLink tag={Link} to='/doctors'>Доктора</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink tag={Link} to='/patients'>Пациенты</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink tag={Link} to='/addVisits'>Записать пациента</NavLink>
-                    </NavItem>
-                    </>
-                ) : user?.roles.includes("Doctor") ? (
-                    <>
-                    <NavItem>
-                        <NavLink tag={Link} to='/patientsCard'>Медкарта</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink tag={Link} to='/doctorsTalons'>Принять пациента</NavLink>
-                    </NavItem>
-                    </>
-                ) : null }
-            </Nav>
-            <UncontrolledDropdown nav>
-                <DropdownToggle caret color="dark">
-                    Аккаунт
-                </DropdownToggle>
-                <DropdownMenu dark>
-                    <DropdownItem text>{user ? user.email : 'Не авторизован'}</DropdownItem>
-                    <DropdownItem tag={Link} to='/register' disabled={user ? true : false}>Регистрация</DropdownItem>
-                    <DropdownItem tag={Link} to='/login' disabled={user ? true : false}>Вход</DropdownItem>
-                    <DropdownItem tag={Link} to='/logout' disabled={user ? false : true}>Выход</DropdownItem>
-                </DropdownMenu>
-            </UncontrolledDropdown>
-        </Navbar>
-    <Outlet />
-    </>
+    <LayoutAntd>
+        <Header style={{display: "flex", position: "sticky", top: 0, zIndex: 1, width: "100%"}}>
+        <Menu theme="dark" mode="horizontal" items={user?.roles.includes("Registrator") ? registratorItems : user?.roles.includes("Doctor") ? doctorItems : [   ]}></Menu>
+            <div style={{marginLeft: 'auto'}}>
+                <UncontrolledDropdown>
+                    <DropdownToggle caret color="dark" right>
+                        Аккаунт
+                    </DropdownToggle>
+                    <DropdownMenu dark right>
+                        <DropdownItem text>{user ? user.email : 'Не авторизован'}</DropdownItem>
+                        <DropdownItem tag={Link} to='/register' disabled={user ? true : false}>Регистрация</DropdownItem>
+                        <DropdownItem tag={Link} to='/login' disabled={user ? true : false}>Вход</DropdownItem>
+                        <DropdownItem tag={Link} to='/logout' disabled={user ? false : true}>Выход</DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+            </div>
+            </Header>
+        <Content className="site-layout" style={{minHeight: "100%"}}>
+            <Outlet />
+        </Content>
+        <Footer style={{textAlign: "center"}}>МИС ©️2024</Footer>
+    </LayoutAntd>
     );
 }
 export default Layout;
