d21a8fd921298cbac9b85901c74df270188742c1
Mark <vernidub66@gmail.com>
2024-04-19 16:49:37 +0300

    Updated axios interceptors

--- Polyclinic_ASP/client/src/Axios/axiosInstance.ts
+++ Polyclinic_ASP/client/src/Axios/axiosInstance.ts
@@ -1,20 +1,28 @@
 import axios from "axios";
+import { notification } from "antd";
 
 const Fetch = axios.create({
-    baseURL: 'http://localhost:8555/',
-    headers: {
-        Accept: 'application/json'
-    },
-    withCredentials: true
-})
+  baseURL: "http://localhost:8555/",
+  headers: {
+    Accept: "application/json",
+  },
+  withCredentials: true,
+});
 
-Fetch.interceptors.response.use(response => response, 
-    error => {
-        console.log(error.response.status);
-        if (error.response.status === 401)
-            console.log("У вас нет доступа к этому ресурсу!");
-        return Promise.reject(error);
+Fetch.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log(error.response.status);
+    if (error.response.status === 401) {
+      notification.error({
+        message: "У вас нет доступа к этому ресурсу! Зарегистрируйтесь или войдите в аккаунт!",
+        placement: "topRight",
+        duration: 100,
+      });
+      console.log("У вас нет доступа к этому ресурсу!");
     }
+    return Promise.reject(error);
+  }
 );
 
 export default Fetch;
