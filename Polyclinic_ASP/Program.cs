1f5cf2347fa5856846fcdf841115a3fc65702820
Mark <vernidub66@gmail.com>
2024-07-03 14:31:15 +0300

    Cookie settings

--- Polyclinic_ASP/Program.cs
+++ Polyclinic_ASP/Program.cs
@@ -58,8 +58,8 @@ builder.Host.UseSerilog();
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
-    options.Cookie.SameSite = SameSiteMode.Strict;
-    options.Cookie.SecurePolicy = CookieSecurePolicy.None;
+    options.Cookie.SameSite = SameSiteMode.None;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
     options.Cookie.HttpOnly = true;
     options.Cookie.Name = "PolyclinicCookie";
     options.LoginPath = "/";
